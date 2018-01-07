87fb49338e0a22eddc2d8c7b522ca270603cf6c6
rivkind <alex.work83@gmail.com>
2018-01-07 16:26:23 +0300

    test

--- Mobile/__tests__/MobileCompany-test.js
+++ Mobile/__tests__/MobileCompany-test.js
@@ -2,19 +2,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import renderer from 'react-test-renderer';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-import { shallow } from 'enzyme';
 
 import MobileCompany from '../components/MobileCompany';
 
-test('работа MobileClient', () => {
+test('работа MobileCompany', () => {
 
-
-  
   const filterName='Все';
   const clientsArr=[ 
     {id:101, fio:"Иванов И.И.", balance:200}, 
@@ -22,8 +14,36 @@ test('работа MobileClient', () => {
     {id:110, fio:"Петров П.П.", balance:180},
     {id:120, fio:"Григорьев Г.Г.", balance:220},
   ];
-  const wrapper = shallow(<MobileCompany name={filterName}
-    clients={clientsArr} />);
-    
-    
+
+  const component = renderer.create(
+    <MobileCompany name={filterName}
+    clients={clientsArr} />
+  );
+
+  let testInstance = component.root;
+  expect(testInstance.findAllByProps({className: "MobileClient"}).length).toEqual(4);
+
+  component.getInstance().deleteClient(110);
+  testInstance = component.root;
+  expect(testInstance.findAllByProps({className: "MobileClient"}).length).toEqual(3);
+
+  component.getInstance().nameClient("Ривкинд Алексей");
+  testInstance = component.root;
+  expect(testInstance.findAllByProps({className: "MobileClient"}).length).toEqual(4);
+
+  component.getInstance().nameClient("Локтев Алексей");
+  testInstance = component.root;
+  expect(testInstance.findAllByProps({className: "MobileClient"}).length).toEqual(5);
+
+  component.getInstance().edClient(105);
+  component.getInstance().nameClient("Петрович П.П.");
+  testInstance = component.root;
+  expect(testInstance.findAllByProps({className: "MobileClient"}).length).toEqual(5);
+
+  //component.toJSON().children[0].children[2].props.onClick();
+  //testInstance = component.root;
+  //expect(testInstance.findAllByProps({className: "MobileClient"}).length).toEqual(4);
+
+
+  
 });
