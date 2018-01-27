4369654266c19bd28d590ddb16fc7572af844d50
Alexey Rivkind <alexey.rivkind@life.com.by>
2018-01-27 19:42:03 +0300

    numword

---
+++ Numword/src/app/numword.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name:"numword",
+  pure:true,
+})
+export class ApplesPipe {
+
+  transform(num:number,word1:string,word2:string,word5:string):string {
+    var str:any=num;
+    var s:any = /^\d+$/;
+    if(!s.test(str)) return '';
+    var dd=num%100;
+    if ( (dd>=11) && (dd<=19) )
+        return num+' '+word5;
+    var d=num%10;
+    if ( d==1 )
+        return num+' '+word1;
+    if ( (d>=2) && (d<=4) )
+        return num+' '+word2;
+    return num+' '+word5;
+  }
+
+}
